37387253708931b759f0ac54715b3319fd5943a7
Jayson Lauza <lauzajayson@gmail.com>
2025-11-28 18:40:09 +0000

    coment google ai

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,10 @@
 import {genkit} from 'genkit';
 import openAI from 'genkitx-openai';
+// import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [openAI({ apiKey: process.env.OPENAI_API_KEY })],
   model: 'gpt-4', // or gpt-4-32k, etc.
+  // plugins: [googleAI()],
+  // model: 'googleai/gemini-2.5-flash',  
 });
